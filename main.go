7c219f5e0f6a49dde0a07a1bc374b9e713e63822
Brandon Locke <btl33@drexel.edu>
2017-11-29 19:34:06 -0500

    Built the map layers structure and the properties structure.

--- main.go
+++ main.go
@@ -15,15 +15,37 @@ import (
 const mapHeight int = 16 //map is mapHeight x mapHeight tiles
 const pixelHeight int = 32 //every tile is pixelHeight x pixelHeight pixels
 
-type mapObject struct {
-	height int
-	layers []mapLayer
-	nextobjectid int
-	orientation string
-	renderorder string
-	tiledversion string
-	tileheight int
-	
+type mapObject struct { //object that holds the properties of the map itself
+	Height int `json:"height"`
+	Layers []mapLayer `json:"layers"`
+	Nextobjectid int `json:"nextobjectid"`
+	Orientation string `json:"orientation"`
+	Properties property `json:"properties"`
+	Renderorder string `json:"renderorder"`
+	Tiledversion string `json:"tiledversion"`
+	Tileheight int `json:"tileheight"`
+	Tilewidth int `json:"tilewidth"`
+	Type string `json:"type"`
+	Version int `json:"version"`
+	Width int `json:"width"`
+}
+
+type mapLayer struct { //each map has an associated layer
+	Data []int `json:"data"`
+	Height int `json:"height"`
+	Name string `json:"name"`
+	Opacity int `json:"opacity"`
+	Properties property `json:"properties"`
+	Type string `json:"type"`
+	Visible bool `json:"visible"`
+	Width int	`json:"width"`
+	X int `json:"x"`
+	Y int `json:"y"`
+	sprite *pixel.Sprite
+}
+
+type property struct { //each map and layer has a set of properties that it can hold
+	Collision bool
 }
 
 func run() {
